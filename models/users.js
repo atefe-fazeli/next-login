85a6caf6bc2f5407278933beb79394da7211e489
atefe fazeli <atefefazeli1996@outlook.com>
2025-05-02 13:23:11 +0330

    create user model

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+});
+const usersModel = mongoose.models.user || mongoose.model("user", schema);
+export default usersModel;
